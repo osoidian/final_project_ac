387dee45284e3083dc56e3df6e9e19e63445ada0
Alonso Castaneda <osoidian@gmail.com>
2018-09-12 23:46:13 +1000

    Start of timer code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-  // alert('connected')
+  // // alert('connected')
+  // var min = new Date()
+  // var sec = new Date()
+  // var minnow = min.getMinutes()
+  // var secnow = sec.getSeconds()
+  // console.log(minnow + ':' + secnow)
+  var count=30;
+
+  var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+  function timer()
+  {
+    console.log(count=count-1)
+    // if (count <= 0)
+    // {
+    //    clearInterval(counter);
+    //    //counter ended, do something here
+    //    return;
+    // }
+
+  //Do code for showing the number of seconds here
+  }
 })
