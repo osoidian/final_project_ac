f8c41144e7b5a5004c7093eae93a7275d4f2856f
Alonso Castaneda <osoidian@gmail.com>
2018-09-16 20:52:17 +1000

    Still trying to figure timer

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-  var customers = []
-  var count = 30
+  $(document).ready(function(){
+    var customers = []
+
+    $('#reserve').click(function(){
+      event.preventDefault()
+
+      var newCustomer = new Object();
+      newCustomer.name = $('#customer-name').val()
+      newCustomer.count = 5
+      // var count = 30
+      var timeAdded = new Date()
+      // var newCustomer = $('#customer-name').val()
+      customers.push(newCustomer)
+      $('#customers-waiting').append('<li>' + newCustomer.name + ' ' + '<span>' + timeAdded.getHours() + ':' + timeAdded.getMinutes() + '</span>' + '<p>' + newCustomer.count + '</p>' + '</li>')
+      $('#customer-name').val('')
   
-  $('#reserve').click(function(){
-    event.preventDefault()
-    var newCustomer = $('#customer-name').val()
-    customers.push(newCustomer)
-    $('#customers-waiting').append('<li>' + newCustomer + ' ' + '<span>' + count + '</span>' + '</li>')
-    $('#customer-name').val('')
-    customers.forEach(function(){
       setInterval(function(){
-        $('span').text(count -= 1)
+        $('p').text(newCustomer.count -= 1)
+        if (newCunstomer.count < 0) {
+          
+        }
       }, 1000)
     })
   })
+  
+  // var count = 30
+  
 
-  // var count=setInterval(timer, 1000);
-  //   function timer(){
-  //     console.log(count -= 1)
-  //   }
-// var min = new Date()
-  // var sec = new Date()
-  // var minnow = min.getMinutes()
-  // var secnow = sec.getSeconds()
-  // console.log(minnow + ':' + secnow)
-
-  // var count=30;
-
-  // var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
-
-  // function timer()
-  // {
-  //   console.log(count=count-1)
-  //   // if (count <= 0)
-  //   // {
-  //   //    clearInterval(counter);
-  //   //    //counter ended, do something here
-  //   //    return;
-  //   // }
-
-  // //Do code for showing the number of seconds here
-  // }
+  // // Working code
+  // $('#reserve').click(function(){
+  //   event.preventDefault()
+    
+  //   var newCustomer = $('#customer-name').val()
+  //   customers.push(newCustomer)
+  //   $('#customers-waiting').append('<li>' + newCustomer + ' ' + '<span>' + count + '</span>' + '</li>')
+  //   $('#customer-name').val('')
+  //   customers.forEach(function(){
+  //     setInterval(function(){
+  //       $('span').text(count -= 1)
+  //     }, 1000)
+  //   })
+  // })
