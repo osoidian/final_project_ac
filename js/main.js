28d37bafa0011c4c8892f4fc37736614a1cee89d
Alonso Castaneda <osoidian@gmail.com>
2018-09-18 19:51:34 +1000

    Virtual queue working!!!

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
   $(document).ready(function(){
     var customers = []
-
+    var customerId = 1
     $('#reserve').click(function(){
       event.preventDefault()
 
-      var newCustomer = new Object();
-      newCustomer.name = $('#customer-name').val()
-      newCustomer.count = 5
-      // var count = 30
-      var timeAdded = new Date()
-      // var newCustomer = $('#customer-name').val()
+      var newCustomer = {
+        id: customerId,
+        name: $('#customer-name').val(),
+        count: 10
+      }
       customers.push(newCustomer)
-      $('#customers-waiting').append('<li>' + newCustomer.name + ' ' + '<span>' + timeAdded.getHours() + ':' + timeAdded.getMinutes() + '</span>' + '<p>' + newCustomer.count + '</p>' + '</li>')
+      var date = new Date()
+      var $customer = $("<li data-customer-id='" + newCustomer.id + "'>")
+      var $timeAdded = $('<span>').text(date.getHours() + ':' + date.getMinutes())
+      var $customerTimer = $('<p>').text(newCustomer.count)
+      $customer.append(newCustomer.name + ' ')
+      $customer.append($timeAdded)
+      $customer.append($customerTimer)
+      $('#customers-waiting').append($customer)
       $('#customer-name').val('')
   
-      setInterval(function(){
-        $('p').text(newCustomer.count -= 1)
-        if (newCunstomer.count < 0) {
-          
+      var intervalId = setInterval(function(){
+        $('li[data-customer-id="' + newCustomer.id + '"] p').text(newCustomer.count -= 1)
+        if (newCustomer.count <= 0) {
+          clearInterval(intervalId)
         }
       }, 1000)
+      customerId++
     })
   })
   
